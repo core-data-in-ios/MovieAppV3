bd386adbf404afc489be32fe0da0cb38a4e66953
Chris Hand <cj.hand@gmail.com>
2022-09-27 23:12:12 -0700

    Make a new screen

---
+++ MovieApp/Screens/ActorDetailsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ActorDetailsScreen.swift
+//  MovieApp
+//
+//  Created by Chris Hand on 9/27/22.
+//
+
+import SwiftUI
+
+struct ActorDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActorDetailsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ActorDetailsScreen()
+    }
+}
